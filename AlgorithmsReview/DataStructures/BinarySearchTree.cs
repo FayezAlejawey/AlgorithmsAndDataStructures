cd0619c686cf2990ccead437f32ebcd52380755a
Fayez <fayez.ejawey@teklabz.com>
2020-12-06 11:15:50 +0200

    Rearranging files + Edit the Delete node logic in BST

---
+++ AlgorithmsReview/DataStructures/BinarySearchTree.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsReview.DataStructures {
+    class BinarySearchTree {
+
+        private Node _rootNode;
+
+        public BinarySearchTree(Node rootNode = null) {
+            _rootNode = rootNode;
+        }
+
+        public void InsertNode(int key) {
+
+            if (_rootNode == null) {
+                _rootNode = new Node(key);
+                return;
+            }
+
+            Insert(key, _rootNode);
+
+        }
+
+        private void Insert(int key, Node parentNode) {
+
+            if (key < parentNode.Key) {
+
+                if (parentNode.LeftNode == null) {
+                    parentNode.LeftNode = new Node(key);
+                    return;
+                }
+
+                Insert(key, parentNode.LeftNode);
+                return;
+
+            }
+
+            if (parentNode.RightNode == null) {
+                parentNode.RightNode = new Node(key);
+                return;
+            }
+
+            Insert(key, parentNode.RightNode);
+
+        }
+
+        public void DeleteNode(int key) {
+
+            if (_rootNode == null) {
+                return;
+            }
+
+            Delete(key, _rootNode, null);
+
+        }
+
+        private void Delete(int key, Node node, Node parentNode) {
+
+            if (node.Key == key) {
+
+                var leftNode = node.LeftNode == null ? null : new Node(node.LeftNode);
+                var rightNode = node.RightNode == null ? null : new Node(node.RightNode);
+
+                if (parentNode is null) {
+
+                    _rootNode = null;
+
+                } else {
+
+                    if (parentNode.LeftNode == node) {
+                        parentNode.LeftNode = null;
+                    } else {
+                        parentNode.RightNode = null;
+                    }
+                }
+
+                InsertAllNodes(leftNode);
+                InsertAllNodes(rightNode);
+
+                return;
+
+            }
+
+            if (key < node.Key) {
+
+                if (node.LeftNode is null) {
+                    return;
+                }
+
+                Delete(key, node.LeftNode, node);
+
+                return;
+            }
+
+            if (node.RightNode is null) {
+                return;
+            }
+
+            Delete(key, node.RightNode, node);
+
+        }
+
+        private void InsertAllNodes(Node node) {
+
+            if (node == null) {
+                return;
+            }
+
+            InsertNode(node.Key);
+            InsertAllNodes(node.LeftNode);
+            InsertAllNodes(node.RightNode);
+
+        }
+
+        public int Max() {
+
+            if (_rootNode == null) {
+                throw new Exception("There is no node in the BST");
+            }
+
+            return TraverseToTheMostRight(_rootNode);
+
+        }
+
+        private int TraverseToTheMostRight(Node node) {
+
+            if (node.RightNode == null) {
+                return node.Key;
+            }
+
+            return TraverseToTheMostRight(node.RightNode);
+
+        }
+
+        public int Min() {
+
+            if (_rootNode == null) {
+                throw new Exception("There is no node in the BST");
+            }
+
+            return TraverseToTheMostLeft(_rootNode);
+
+        }
+
+        private int TraverseToTheMostLeft(Node node) {
+
+            if (node.LeftNode == null) {
+                return node.Key;
+            }
+
+            return TraverseToTheMostLeft(node.LeftNode);
+
+        }
+
+        void PrintPostOrder(Node node) {
+
+            if (node == null)
+                return;
+
+            // first recur on left subtree 
+            PrintPostOrder(node.LeftNode);
+
+            // then recur on right subtree 
+            PrintPostOrder(node.RightNode);
+
+            // now deal with the node 
+            Console.Write(node.Key + " ");
+
+        }
+
+        void PrintInOrder(Node node) {
+            if (node == null)
+                return;
+
+            /* first recur on left child */
+            PrintInOrder(node.LeftNode);
+
+            /* then print the data of node */
+            Console.Write(node.Key + " ");
+
+            /* now recur on right child */
+            PrintInOrder(node.RightNode);
+
+        }
+
+        void PrintPreOrder(Node node) {
+            if (node == null)
+                return;
+
+            /* first print data of node */
+            Console.Write(node.Key + " ");
+
+            /* then recur on left sutree */
+            PrintPreOrder(node.LeftNode);
+
+            /* now recur on right subtree */
+            PrintPreOrder(node.RightNode);
+
+        }
+
+        public void PrintBst() {
+            if (_rootNode is null) {
+                Console.WriteLine("No BST Data.");
+                return;
+            }
+
+            _rootNode.PrintPretty("", true);
+
+        }
+    }
+    class Node {
+        public int Key { get; set; }
+        public Node LeftNode { get; set; }
+        public Node RightNode { get; set; }
+
+        public Node(int key) {
+            Key = key;
+        }
+
+        public Node(Node node) {
+            Key = node.Key;
+            LeftNode = node.LeftNode;
+            RightNode = node.RightNode;
+        }
+
+        public void PrintPretty(string indent, bool last) {
+
+            Console.Write(indent);
+            if (last) {
+                Console.Write("└─");
+                indent += "  ";
+            } else {
+                Console.Write("├─");
+                indent += "| ";
+            }
+            Console.WriteLine(Key);
+
+            var children = new List<Node>();
+            if (LeftNode != null)
+                children.Add(LeftNode);
+            if (RightNode != null)
+                children.Add(RightNode);
+
+            for (int i = 0; i < children.Count; i++)
+                children[i].PrintPretty(indent, i == children.Count - 1);
+
+        }
+    }
+}
---
+++ AlgorithmsReview/DataStructures/Graph.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+
+namespace AlgorithmsReview.DataStructures {
+    class Graph<T> {
+
+        public Dictionary<T, LinkedList<T>> Adjecency { get; } = new Dictionary<T, LinkedList<T>>();
+
+        public void AddVertex(T vertex) {
+            Adjecency[vertex] = new LinkedList<T>();
+        }
+
+        public void AddEdge(T fromVertex, T toVertex) {
+
+            if (Adjecency.ContainsKey(fromVertex) && Adjecency.ContainsKey(toVertex)) {
+                Adjecency[fromVertex].AddLast(toVertex);
+            }
+        }
+
+        //O(V+E)
+        //O(V) Space Complexity
+        public LinkedList<T> BreadthFirstSearch(T start) {
+
+            var visited = new LinkedList<T>();
+            if (!Adjecency.ContainsKey(start)) {
+                return visited;
+            }
+
+            Queue<T> queue = new Queue<T>();
+            queue.Enqueue(start);
+
+            while (queue.Count > 0) {
+
+                T vertex = queue.Dequeue();
+                if (visited.Contains(vertex)) {
+                    continue;
+                }
+
+                visited.AddLast(vertex);
+
+                foreach (T item in Adjecency[vertex]) {
+
+                    if (!visited.Contains(item)) {
+                        queue.Enqueue(item);
+                    }
+                }
+            }
+
+            return visited;
+
+        }
+
+        //O(V+E)
+        //O(V) Space Complexity
+        public LinkedList<T> DepthFirstSearch(T start) {
+
+            var visited = new LinkedList<T>();
+            if (!Adjecency.ContainsKey(start)) {
+                return visited;
+            }
+
+            Stack<T> stack = new Stack<T>();
+            stack.Push(start);
+
+            while (stack.Count > 0) {
+
+                T vertex = stack.Pop();
+                if (visited.Contains(vertex)) {
+                    continue;
+                }
+
+                visited.AddLast(vertex);
+
+                foreach (T item in Adjecency[vertex]) {
+
+                    if (!visited.Contains(item)) {
+                        stack.Push(item);
+                    }
+                }
+            }
+
+            return visited;
+
+        }
+
+        public string ShortestPath(T startVertex, T endVertex) {
+
+            if (!Adjecency.ContainsKey(startVertex) || !Adjecency.ContainsKey(endVertex)) {
+                return string.Empty;
+            }
+
+            var visited = new List<T>();
+            Queue<(T, string)> queue = new Queue<(T, string)>();
+            queue.Enqueue((startVertex, startVertex.ToString()));
+
+            while (queue.Count > 0) {
+
+                var vertex = queue.Dequeue();
+                if (visited.Contains(vertex.Item1)) {
+                    continue;
+                }
+                visited.Add(vertex.Item1);
+
+                if (Equals(vertex.Item1, endVertex)) {
+                    return vertex.Item2;
+                }
+
+                foreach (T edge in Adjecency[vertex.Item1]) {
+
+                    if (visited.Contains(edge)) {
+                        continue;
+                    }
+
+                    queue.Enqueue((edge, vertex.Item2 + ", " + edge.ToString()));
+
+                }
+            }
+
+            return string.Empty;
+
+        }
+    }
+}
